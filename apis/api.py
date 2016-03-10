f00d88929a8639d34c707f0723602a36573e90cd
carlos8410 <vincent.python.2015@gmail.com>
2016-03-09 23:23:47 -0500

    Add the function of generate_plot

--- apis/api.py
+++ apis/api.py
@@ -2,6 +2,8 @@ FILE_PATH = ''
 import urllib2
 import logging
 import requests
+import matplotlib.pyplot as plt 
+import numpy
 
 CPI_DATA_URL = 'https://research.stlouisfed.org/fred2/data/CPIAUCSL.txt'
 Headline_of_Data = "DATE          VALUE"
@@ -206,6 +208,70 @@ def is_valid_dataset(platform):
         logging.warn(u"%s has no abbreviation" % platform['name'])
         return False
     return True
+
+def generate_plot(platforms, output_file):
+    """Generates a bar chart out of the given platforms and writes the output
+    into the specified file as PNG image.
+
+    """
+    # First off we need to convert the platforms in a format that can be
+    # attached to the 2 axis of our bar chart. "labels" will become the
+    # x-axis and "values" the value of each label on the y-axis:
+    labels = []
+    values = []
+    for platform in platforms:
+        name = platform['name']
+        price = platform['original_price']
+        adapted_price = platform['adjusted_price']
+
+        # Skip price higher than 2000 USD simply because it would make the output unusable.
+        if price > 2000:
+            continue
+
+        # If the name of the platform is too long, replace it with the
+        # abbreviation. list.insert(0, val) inserts val at the beginning of
+        # the list.
+        if len(name) > 15:
+            name = platform['abbreviation']
+        lables.insert(0, u"%s\n$ %s\n$ %s" % (name, price, round(adjusted_price, 2)))
+        values.insert(0, adjusted_price)
+
+    # Define the width of each bar and the size of the resulting graph.
+    width = 0.3
+    ind = numpy.arange(len(values))
+    fig = plt.figure(figsize=(len(labels) * 1.8, 10))
+
+    # Generate a subplot and put our values onto it.
+    ax = fig.add_subplot(1, 1, 1)
+    ax.bar(ind, values, width, align='center')
+
+    # Format the X and Y axis labels. Also set the ticks on the x-axis slightly
+    # farther apart and give then a slight tilting effect.
+    plt.ylabel('Adjusted Price')
+    plt.xlabel('Year / Console')
+    ax.set_xticks(ind + 0.3)
+    ax.set_xticklabels(labels)
+    fig.autofmt_xdate()
+    plt.grid(True)
+    plt.savefig(output_file, dpi = 72)
+
+
+def visualize_type(parsed_data):
+    """Visualize data by category in a bar graph"""
+    counter = Counter(item['Category'] for item in parsed_data)
+    
+    width = 0.5 # Width of each bar
+    labels = tuple(counter.keys())
+    xlocations = numpy.arange(len(labels)) + 0.5
+    plt.bar(xlocations, counter.values(), width=width)
+    plt.xticks(xlocations + width / 2, labels, rotation=90)
+    # Give some more room so the labels aren't cut off in the graph
+    plt.subplots_adjust(bottom = 0.4)
+    # Make the overall graph/figure larger
+    plt.rcParams['figure.figsize'] = 12, 8
+
+    plt.savefig("Types.png")
+    plt.clf()
 
 
 if __name__ == "__main__":
